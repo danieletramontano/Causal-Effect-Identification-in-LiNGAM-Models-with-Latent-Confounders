7af1baa68c18d422dce21097d9744123d18d90f1
Daniele Tramontano <88477195+danieletramontano@users.noreply.github.com>
2023-11-13 15:56:24 +0100

    Update functions.py

--- functions.py
+++ functions.py
@@ -11,9 +11,9 @@ import numpy as np
 import torch
 from torch.nn import Parameter
 import matplotlib.pyplot as plt
-
-
-
+import random 
+import math
+import pandas as pd
 
 def hid_var_dag(latent=2,observed=5,dag_type = 'default',parents_max = 3,
                 expected_degree=3):
@@ -65,9 +65,9 @@ def hid_var_dag(latent=2,observed=5,dag_type = 'default',parents_max = 3,
 
 
 def hid_var_data(latent=2,observed=5,g = None, n = 500, 
-                 withening = True):
+                 withening = True, distr = 'Laplace', same_var = True):
     """
-    Generate whitened data from a given DAG.
+    Generate, possibly whitened, data from a given DAG.
     
     Parameters:
         - latent (int): Number of hidden variables.
@@ -75,24 +75,44 @@ def hid_var_data(latent=2,observed=5,g = None, n = 500,
         - g (nx.DiGraph): The DAG as a NetworkX DiGraph object.
         - n (int): Number of data samples to generate.
         - withening (bool): Flag to apply whitening to the data.
+        - distr(string): 'Laplace' or 'Cauchy'
+        - same_var(bool): True if the noise terms must have the same variance
     
     Returns:
         - data (torch.Tensor): Generated data.
         - adj (torch.Tensor): Adjacency matrix of the DAG.
-        - W_with (torch.Tensor): Whitening matrix (if applicable).
+        - W_with (torch.Tensor): Whitening matrix (if applicable otherwise identity matrix).
         - weight (torch.Tensor): Weights associated with the DAG edges.
     
     """
-    weight =10*torch.Tensor(len(g.edges())).normal_()
+    weight =torch.Tensor(len(g.edges())).normal_(0,10)
     
     adj = torch.eye(len(g.nodes()))
     for e in range(len(g.edges())):
         adj[list(g.edges)[e]]=-weight[e]
     B = (torch.inverse(adj)).t()
+
+    
+    err = torch.Tensor(latent+observed,n)
+    
+    for j in range(latent+observed):
+        if same_var == 'True':
+            varr = 1
+        else:
+            varr = torch.Tensor(1).uniform_(0.2,10)
+        for i in range(n):
+            if distr == 'Laplace':
+                err[j,i] = torch.distributions.laplace.Laplace(0,1).sample()*varr
+            elif distr == 'Cauchy ':
+                err[j, i] = torch.Tensor(1).cauchy_()*varr
+            else:
+                raise("'Distr' has to be either 'Laplace' of 'Cauchy'") 
+                
+
     
     err = torch.Tensor(latent+observed,n).cauchy_()
     data = B.matmul(err).t()
-    data = data[:,latent:(observed+1)]
+    data = data[:,range(latent, observed+latent)]
     d_cov = (data.t()).cov()
     
     if withening == True:
@@ -106,29 +126,58 @@ def hid_var_data(latent=2,observed=5,g = None, n = 500,
     return data, adj,torch.eye(observed) ,weight
 
 
-def graphical_rica(latent, observed, g, data, moms, lambdas, epochs, lr, W_with, pseudoinverse, w_init, w_true):
+def init_w_guess(data, g, latent, observed, W_with):
+    up_data = W_with.inverse().matmul(data.t())
+    w = torch.zeros(len(g.edges()))
+    mask = torch.zeros(len(g.edges()))
+
+    for i, e in enumerate(g.edges()):
+        if e[0] < latent:
+            w[i] = torch.Tensor(1).normal_().item()
+            mask[i] = 1
+        else:
+            G_cov = up_data.cov()
+            w[i] = G_cov[e[0]-latent,e[1]-latent]/G_cov[e[0]-latent,e[0]-latent]
+            up_data[:, e[1]-latent] = up_data[:, e[1]-latent]-w[i]*up_data[:,e[0]-latent]
+
+            an_s = sorted(nx.ancestors(g, e[0]))
+            i_s = intersection(an_s,list(range(latent)))
+
+            if len(i_s) > 0 :
+                an_t = sorted(nx.ancestors(g, e[1]))
+                i_t = intersection(an_t,list(range(latent)))
+                if len(i_t)>0:
+                    ints = intersection(i_s,i_t)
+                    if len(ints)>0:
+                        mask[i] = 1
+    return w, mask    
+
+
+def intersection(lst1, lst2):
+    lst3 = [value for value in lst1 if value in lst2]
+    return lst3
+
+def graphical_rica(latent, observed, g, data, moms, lambdas, epochs, lr, W_with, w_init, w_true):
     
     """
-        Perform the optimization task for graphical Independent Component Analysis (ICA).
+        Graphical adaptation of RICA
         
         Parameters:
             - latent (int): Number of hidden variables.
             - observed (int): Number of observed variables.
             - g (nx.DiGraph): The DAG as a NetworkX DiGraph object.
             - data (torch.Tensor): Input data.
+            - lr(double): Learning rate of the optimizer.
             - moms (list): List of momentum values for optimization.
             - lambdas (list): List of regularization lambda values for optimization.
             - epochs (int): Number of optimization epochs.
             - W_with (torch.Tensor): Whitening matrix.
-            - pseudoinverse (bool): Flag to use pseudoinverse.
-            - w_init (str): Weight initialization strategy ('random' or 'true').
+            - w_init (str): Weight initialization strategy ('random', 'true', 'cov_guess').
             - w_true (torch.Tensor): True weights of the DAG edges.
         
         Returns:
             - loss_data (torch.Tensor): Loss data during optimization.
-            - loss_recon_data (torch.Tensor): Reconstruction loss data during optimization.
-            - loss_latent_data (torch.Tensor): Latent loss data during optimization.
-            - w_loss_data (torch.Tensor): Weight loss data during optimization.
+            - w_loss_data (torch.Tensor): Squared distance of the difference between the true and the estimated parameters during optimization.
             
         """
     
@@ -143,10 +192,14 @@ def graphical_rica(latent, observed, g, data, moms, lambdas, epochs, lr, W_with,
         for j in range(len(lambdas)):
             
             if w_init == 'random':
-                weight = Parameter(torch.Tensor(len(g.edges())).normal_())
+                weight = Parameter(torch.Tensor(len(g.edges())).normal_(0,4))
+            elif w_init == 'cov_guess':
+                w, mask = init_w_guess(data, g, latent, observed, W_with)
+                weight = Parameter(w[mask == 1])
+                fix_weight = w[mask == 0]        
+                c_list = count_lists(mask)
             else :
                 weight = Parameter(torch.clone(w_true).detach().requires_grad_(True))
-            
             mom = moms[i]
             lambd = lambdas[j]
             optimizer = torch.optim.RMSprop([weight], lr, momentum=mom)
@@ -155,15 +208,20 @@ def graphical_rica(latent, observed, g, data, moms, lambdas, epochs, lr, W_with,
             for epoch in range(epochs):
                     
                 adj = torch.eye(len(g.nodes()))
-                for e in range(len(g.edges())):
-                    adj[list(g.edges)[e]]=-weight[e]
-                
+                if w_init == 'cov_guess':
+                    for ii, e in enumerate(g.edges()):
+                        if mask[ii] == 1:
+                            adj[e]=-weight[int(c_list[ii])]
+                        else:
+                            adj[e]=-fix_weight[int(c_list[ii])]
+                    
+                else:
+                    for e in range(len(g.edges())):
+                        adj[list(g.edges)[e]]=-weight[e]
+
                 B = (torch.inverse(adj)).t()
                 B = B[latent:latent+observed,:]
                 B = W_with.matmul(B)
-                
-                if pseudoinverse == True:
-                    B = B.matmul((B.t().matmul(B)).inverse())
 
                 latents = data.matmul(B)
                 output = latents.matmul(B.t())
@@ -171,25 +229,45 @@ def graphical_rica(latent, observed, g, data, moms, lambdas, epochs, lr, W_with,
                 
                 diff = output - data
                 loss_recon = (diff * diff).mean()
+                #loss_recon = diff.abs().mean()
+                #loss_recon = diff.cosh().log().mean()
                 loss_latent = latents.abs().mean()
+                #loss_latent = latents.cosh().log().mean()
                 loss =lambd * loss_recon + loss_latent
                 
-                
+
                 loss_data[i, j, epoch] =  (loss.data).item()
-                loss_recon_data[i, j, epoch] =  (loss_recon.data).item()
-                loss_latent_data[i, j, epoch] =  (loss_latent.data).item()
+
                
-                w_loss_data[:,i,j, epoch] = (weight-torch.tensor(w_true)).square()       
+                if  w_init == 'cov_guess':
+                    #w_loss_data[mask==1,i,j, epoch] = (weight-torch.tensor(w_true[mask==1])).square()
+                    w_loss_data[mask==1,i,j, epoch] = (weight-w_true[mask==1].clone().detach()).square()
+                    #w_loss_data[mask==0,i,j, epoch] = (fix_weight-torch.tensor(w_true[mask==0])).square()
+                    w_loss_data[mask==0,i,j, epoch] = (fix_weight-w_true[mask==0].clone().detach()).square()
+                else:
+                    w_loss_data[:,i,j, epoch] = (weight-w_true.clone().detach()).square()       
                 
                 optimizer.zero_grad()
                 loss.backward()
-                optimizer.step()
-                
+                optimizer.step()                    
 
-    return loss_data, loss_recon_data, loss_latent_data, w_loss_data
+    return loss_data, w_loss_data
 
-
-def loss_plots(loss_data, loss_recon_data, loss_latent_data, w_loss_data, moms, lambdas, epochs):
+def count_lists(l):
+    c_m = -1
+    c_um = -1
+    c_list = np.zeros(len(l))
+    for i, e in enumerate(l):
+        if l[i] == 0:
+            c_um = c_um+1
+            c_list[i] = c_um
+        else:
+            c_m = c_m+1
+            c_list[i] = c_m
+    return c_list
+    
+    
+def loss_plots(loss_data, w_loss_data, moms, lambdas, epochs, w_true, w_of_int):
     """
     Generate and display plots.
 
@@ -204,6 +282,10 @@ def loss_plots(loss_data, loss_recon_data, loss_latent_data, w_loss_data, moms, 
     """
 
     fig, axs = plt.subplots(len(moms), len(lambdas), figsize=(12, 8))
+    #print(f' True coefficients : {w_true[2:4]}, loss 2stlq: {w_2stg}')
+    print(f' True coefficients : {w_true[w_of_int]}')
+    # Create a list to store legend handles and labels
+    legend_handles, legend_labels = [], []
 
     for i in range(len(moms)):
         for j in range(len(lambdas)):
@@ -211,7 +293,7 @@ def loss_plots(loss_data, loss_recon_data, loss_latent_data, w_loss_data, moms, 
 
             # Plot on the current subplot
             ax1 = axs[i, j]
-            line1, = ax1.plot(x, loss_data[i, j, :], 'b-', label='Loss Data')
+            line1, = ax1.plot(x, loss_data.log()[i, j, :], 'b-', label='Loss Data')
             ax1.set_xlabel('Epochs')
             ax1.set_ylabel('Loss Data', color='b')
             ax1.tick_params('y', colors='b')
@@ -219,18 +301,28 @@ def loss_plots(loss_data, loss_recon_data, loss_latent_data, w_loss_data, moms, 
             # Create a second y-axis on the same subplot
             ax2 = ax1.twinx()
             lines2 = []
-            for k in range(4):
-                line2, = ax2.plot(x, w_loss_data[k, i, j, :].detach().numpy(), linestyle='--', label=f'W Loss {k}')
+            
+            #for k in range(2):
+            for k in w_of_int:
+                line2, = ax2.plot(x, w_loss_data[k, i, j, :].log().detach().numpy(), linestyle='--', label=f'W_{k} Loss')
+                #line2, = ax2.plot(x, w_loss_data[k+2, i, j, :].log().detach().numpy(), linestyle='--', label=f'W_{k} Loss')
                 lines2.append(line2)
+                #line2st, = ax2.plot(x, [w_2stg[k]]*len(x), '-', label=f'W_{k} 2stg Loss')
+                #lines2.append(line2st)
             ax2.set_ylabel('W Loss', color='r')
             ax2.tick_params('y', colors='r')
-
-            # Combine legends from both subplots
-            lines = [line1] + lines2
-            labels = [line.get_label() for line in lines]
-            ax1.legend(lines, labels, loc='upper right')
+            
+            
 
             ax1.set_title(f'Lambda: {lambdas[j]}, Mom: {moms[i]}')
+            print(f'Lambda: {lambdas[j]}, Mom: {moms[i]}, Loss at minimum : {w_loss_data[w_of_int,i,j,loss_data[i, j, :].argmin()].log()}')
+
+    # Create a common legend outside the subplots
+    # Extend the legend handles and labels
+    
+    legend_handles.extend([line1] + lines2)
+    legend_labels.extend(['Loss Data'] + [f'W_{k} Loss' for k in w_of_int])
+    fig.legend(legend_handles, legend_labels, bbox_to_anchor=(0.6, 0.5, 0.5, 0.5))
 
     # Adjust layout and display the plots
     plt.tight_layout()
